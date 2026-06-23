d512c8e02e3feb1ccfd1ed50b4c258f713443e3b
abdnad-ai <289069044+abdnad-ai@users.noreply.github.com>
2026-06-23 15:24:59 +0500

    feat: add task CRUD endpoints with DTOs and validation

---
+++ nestjs-projects/week-3-task-manager-api/src/tasks/dto/update-task.dto.ts
@@ -0,0 +1,16 @@
+import { IsBoolean, IsOptional, IsString, IsNotEmpty } from 'class-validator';
+
+export class UpdateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  title?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsBoolean()
+  @IsOptional()
+  completed?: boolean;
+} 
