39ffced51d909bc12b89c86f659832b17ac6c2cc
abdnad-ai <289069044+abdnad-ai@users.noreply.github.com>
2026-06-24 21:59:36 +0500

    add wee-3-task-manager-api

--- nestjs-projects/week-3-task-manager-api/src/main.ts
+++ nestjs-projects/week-3-task-manager-api/src/main.ts
@@ -4,6 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -11,6 +16,7 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(3001);
 }
-bootstrap(); 
+bootstrap();  
